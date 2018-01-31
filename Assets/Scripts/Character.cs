b9d4099e779905a0f23caed8176a0ed343e25576
sevic69 <sebdeveloper6952@gmail.com>
2018-01-31 12:05:53 -0600

    Salto mejorado.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,9 +6,12 @@ using UnityEngine.UI;
 public class Character : MonoBehaviour
 {
     public Camera cam;
+
+    [Header("Jumping Attributes")]
     public Transform feetPoint;
     public float groundCheckDist;
     public float jumpForce;
+    public int numJumps;
     public LayerMask groundLayer;
 
     private Animator anim;
@@ -18,6 +21,7 @@ public class Character : MonoBehaviour
     private float speed = 5f;
     private bool facingRight = true;
     private bool jumped;
+    private int timesJumped;
 
 	// Use this for initialization
 	void Start ()
@@ -27,6 +31,7 @@ public class Character : MonoBehaviour
         anim = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        timesJumped = 0;
     }
 	
 	// Update is called once per frame
@@ -41,7 +46,7 @@ public class Character : MonoBehaviour
         
         sr.flipX = !facingRight;
 
-        if (Input.GetButtonDown("Jump") && !jumped)
+        if (Input.GetButtonDown("Jump") && ++timesJumped < numJumps)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
             rb2d.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
@@ -54,15 +59,26 @@ public class Character : MonoBehaviour
         anim.SetFloat("Speed", Mathf.Abs(move));
 
 
-        // detect ground contact
+        // metodo 1 para detectar contacto
         DetectPlayerTouchingGround();
 	}
 
+    /// <summary>
+    /// Metodo para detectar si el jugador esta en contacto con algun objeto bajo sus pies.
+    /// </summary>
     private void DetectPlayerTouchingGround()
     {
         Collider2D col = Physics2D.OverlapBox(feetPoint.position, Vector2.one * groundCheckDist, 0f, groundLayer);
-        if (col != null) jumped = false;
-        else jumped = true;
+        if (col != null)
+        {
+            jumped = false;
+            timesJumped = 0;
+        }
+        else
+        {
+            jumped = true;
+        }
+        // set animator parameter
         anim.SetBool("Jump", jumped);
     }
 }
